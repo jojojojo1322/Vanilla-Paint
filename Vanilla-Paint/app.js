39cf5133deeed7da2feffa5e73c379e1648922e5
jojojojo1322 <bobob935@gmail.com>
2020-05-21 17:53:13 +0900

    Fill / Paint change

--- Vanilla-Paint/app.js
+++ Vanilla-Paint/app.js
@@ -2,14 +2,19 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const fill = document.getElementById("jsMode");
 
-ctx.strokeStyle = "#2c2c2c";
+const FIRST_COLOR = "#2c2c2c";
+
+ctx.strokeStyle = FIRST_COLOR;
+ctx.fillStyle = FIRST_COLOR;
 ctx.lineWidth = 2.5;
 
 canvas.width = 700;
 canvas.height = 600;
 
 let painting = false;
+let filling = false;
 
 function startPainting() {
   painting = true;
@@ -22,10 +27,22 @@ function stopPainting() {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
   ctx.lineWidth = event.target.value;
+}
+
+function handleFillChange(event) {
+  if (filling === true) {
+    filling = false;
+    fill.innerText = "FILL";
+  } else {
+    filling = true;
+    fill.innerText = "PAINT";
+    ctx.fillStyle = ctx.strokeStyle;
+  }
 }
 
 function onMouseMove(event) {
@@ -42,17 +59,29 @@ function onMouseMove(event) {
   }
 }
 
+function handleCanvasClick(event) {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
 
+// array가 비어져있을수도 있으므로 if문 사용
 if (range) {
   range.addEventListener("input", handleRangeChange);
+}
+
+if (fill) {
+  fill.addEventListener("click", handleFillChange);
 }
